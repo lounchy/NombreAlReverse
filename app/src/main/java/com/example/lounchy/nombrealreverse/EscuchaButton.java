ae6878d8b06322fd5c725dbf2e8429bb21cf3d44
lounchy <belinskisdainis@gmail.com>
2016-12-14 20:38:22 +0100

    version mejorada

--- app/src/main/java/com/example/lounchy/nombrealreverse/EscuchaButton.java
+++ app/src/main/java/com/example/lounchy/nombrealreverse/EscuchaButton.java
@@ -2,43 +2,127 @@ package com.example.lounchy.nombrealreverse;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 /**
- * Created by Menchu y Dainis on 13/12/2016.
+ * Nueva clase hay que crear si 1. Por su naturaleza 2. Por su funcionalidad
+ * Clase creada para escuchar el button(por su funcionalidad).
  */
 
 public class EscuchaButton implements View.OnClickListener {
     private Context context;
 
     public EscuchaButton(Context context) {
+        Log.v(getClass().getCanonicalName(), "Ha entrado en el constructor EscuchaButton");
         this.context = context;
     }
 
     @Override
     public void onClick(View view) {
+        Log.v(getClass().getCanonicalName(), "Has pulsado el boton");
+
+        /**
+         *No se muy bien que hago aqui, supongo que algo con MainActivity
+         */
         Activity activity = (Activity) context;
+
+        /**
+         * Detecto el esditText en XML
+         */
         EditText editText = (EditText) activity.findViewById(R.id.nombre_edit_text);
-        String nombre = editText.getText().toString();
-        String mensaje = mensajeSalida(nombre);
-        TextView textView = (TextView) activity.findViewById(R.id.generar_text_view);
 
-        if (nombre.length() > 1) {
-            textView.setText(mensaje);
-        } else if (nombre.length() > 16) {
-            textView.setText("Ha producido un error!" + "\n" + "Su nombre es demasiado largo para generarlo.");
+        /**
+         * Defino --sacarNombre--
+         * encuentra el texto de EditText
+         */
+        String sacarNombre = editText.getText().toString();
+
+        Log.d(getClass().getCanonicalName(), "Has introducido el nombre: " + sacarNombre);
+
+        /**
+         * Defino --mensajeFinal--
+         * encuentra el metodo --mensajeSalida-- en la clase StringMensaje
+         */
+        String mensajeFinal = StringMensaje.mensajeSalida(sacarNombre);
+
+        /**
+         *--------------------No lo se que estoy haciendo aqui
+         */
+        mostrarLayout(activity.findViewById(R.id.activity_main));
+
+        /**
+         *Defino --caja_resultado
+         * encuentra LinearLayout con @id/resultado en activity_main
+         */
+        ViewGroup caja_resultado = (ViewGroup) activity.findViewById(R.id.resultado);
+        if (caja_resultado.getChildCount() > 0) {
+
+            /**
+             * Detecto el TextView en XML
+             */
+            TextView textView = (TextView) activity.findViewById(R.id.mensaje_salida);
+
+            /**
+             * Creo 3 opciones de mensajes para diferentes casos
+             */
+            if (sacarNombre.length() > 1 && sacarNombre.length() < 16) {
+                textView.setText(mensajeFinal);
+            } else if (sacarNombre.length() > 16) {
+                textView.setText("Ha producido un error!" + "\n" + "Su nombre es demasiado largo para generarlo.");
+            } else {
+                textView.setText("Ha producido un error!" + "\n" + "Para generar su nombre hace falta introducir más de una letra!");
+            }
+
+
         } else {
-            textView.setText("Ha producido un error!" + "\n" + "Para generar su nombre hace falta introducir más de una letra!");
+
+            /**
+             *--------------------No lo se que estoy haciendo aqui
+             */
+            LayoutInflater layoutInflater = activity.getLayoutInflater();
+
+            /**
+             *
+             */
+            View vista_inflada = layoutInflater.inflate(R.layout.mensaje_salida, caja_resultado);
+
+            /**
+             * Detecto el TextView en XML
+             */
+            TextView textView = (TextView) vista_inflada.findViewById(R.id.mensaje_salida);
+
+            /**
+             * Creo 3 opciones de mensajes para diferentes casos
+             */
+            if (sacarNombre.length() > 1 && sacarNombre.length() < 16) {
+                textView.setText(mensajeFinal);
+            } else if (sacarNombre.length() > 16) {
+                textView.setText("Ha producido un error!" + "\n" + "Su nombre es demasiado largo para generarlo.");
+            } else {
+                textView.setText("Ha producido un error!" + "\n" + "Para generar su nombre hace falta introducir más de una letra!");
+            }
         }
-
-
     }
 
-    private String mensajeSalida(String nombre) {
-        String nombreReverse = new StringBuffer(nombre).reverse().toString();
-        String mensaje = "¡Hola " + nombre +"!"  + "\n" + "Su nombre al reverse es:" + "\n" + nombreReverse;
-        return mensaje;
+    /**
+     *--------------No lo se que estoy haciendo aqui.
+     */
+    private void mostrarLayout(View view) {
+        Log.d(getClass().getCanonicalName(), view.getClass().getCanonicalName());
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                View vistaHija = viewGroup.getChildAt(i);
+                mostrarLayout(vistaHija);
+            }
+        }
     }
+
 }
--- app/src/main/java/com/example/lounchy/nombrealreverse/MainActivity.java
+++ app/src/main/java/com/example/lounchy/nombrealreverse/MainActivity.java
@@ -2,17 +2,45 @@ package com.example.lounchy.nombrealreverse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        /**
+         * IMPORTANTE Y FUNDAMENTAL
+         * usar ---Log--- para un control mejor.
+         */
+        Log.v(getClass().getCanonicalName(), "Ha entrado en el metodo onCreate");
+
+        /**
+         * Llamando el metodo --onCreate-- de ---Padre---
+         */
         super.onCreate(savedInstanceState);
+
+        /**
+         *Dentro de un --onCreate-- siempre hay que poner ---setContentView---
+         * Mostra una XML
+         */
         setContentView(R.layout.activity_main);
 
+        /**
+         * Detecto el boton en XML
+         */
         Button button = (Button)findViewById(R.id.generar_button);
-        EscuchaButton escuchaButton = new EscuchaButton(this);
+
+        /**
+         *Defino la clase --EscuchaButton-- creando nuevo objeto
+         */
+        EscuchaButton escuchaButton = new EscuchaButton(this);      //--this es el objeto de MainActivity (la pantalla)
+
+        /**
+         *De forma explicita acossio al button la clase que se va encargar a escucharlo.
+         */
         button.setOnClickListener(escuchaButton);
+
     }
 }
